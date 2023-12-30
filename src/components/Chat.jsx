ff998c2f8b4ccad407f7fdc7366b4b6e4d0686b0
tariktrokic <tariktrokic@gmail.com>
2023-12-30 23:27:43 +0300

    Add chat header with profile link and update stylesheet

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,9 +5,16 @@ import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
+  const isChatPartnerSelected = data.chatId !== "null";
 
   return (
     <div className="chat">
+      {isChatPartnerSelected && (
+        <header className="chatHeader">
+          <p>{data.user.displayName}</p>
+          <a href={`/profile/${data.user.uid}`}>click here for user profile</a>
+        </header>
+      )}
       <Messages chatData={data} />
       <Input />
     </div>
