0f15a9f3471374cbdf5ceb9029075f2e65275496
tariktrokic <tariktrokic@gmail.com>
2023-12-31 00:28:48 +0300

    Add loading to the user profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -8,14 +8,17 @@ import { db } from "../firebase";
 const UserProfile = () => {
   const { currentUser } = useContext(AuthContext);
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const params = useParams();
   const userId = params.userId;
 
   useEffect(() => {
     const getUserProfile = async () => {
+      setIsLoading(true);
       const docRef = doc(db, "users", userId);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
+        setIsLoading(false);
         setUser(docSnap.data());
       }
     };
@@ -25,25 +28,33 @@ const UserProfile = () => {
 
   return (
     <div className="userProfile">
-      <img src={user?.photoURL} alt="Profile" />
-      <h2>{user?.displayName}</h2>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <img src={user?.photoURL} alt="Profile" />
+          <h2>{user?.displayName}</h2>
 
-      <p
-        style={{ color: "rgb(175, 175, 175)", marginTop: "1rem" }}
-      >{`Price per character for message: ${
-        user?.dollarsPerCharacter ? "$" + user.dollarsPerCharacter : "Not set"
-      }`}</p>
-      <p style={{ color: "rgb(175, 175, 175)" }}>{`Response rate: 85%`}</p>
-      {currentUser && (
-        <Link
-          style={{
-            marginTop: "2rem",
-          }}
-          className="button"
-          to={`/chat/${user?.uid}`}
-        >
-          Chat with {user?.displayName}
-        </Link>
+          <p
+            style={{ color: "rgb(175, 175, 175)", marginTop: "1rem" }}
+          >{`Price per character for message: ${
+            user?.dollarsPerCharacter
+              ? "$" + user.dollarsPerCharacter
+              : "Not set"
+          }`}</p>
+          <p style={{ color: "rgb(175, 175, 175)" }}>{`Response rate: 85%`}</p>
+          {currentUser && (
+            <Link
+              style={{
+                marginTop: "2rem",
+              }}
+              className="button"
+              to={`/chat/${user?.uid}`}
+            >
+              Chat with {user?.displayName}
+            </Link>
+          )}
+        </>
       )}
     </div>
   );
