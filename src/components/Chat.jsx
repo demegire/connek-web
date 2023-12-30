a3eedf07732bb1f6838f7ae912032e191cf9896b
tariktrokic <tariktrokic@gmail.com>
2023-12-30 22:47:59 +0300

    Move use chat context call from Messages to it's parent component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Messages from "./Messages";
 import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
 
   return (
     <div className="chat">
-      <Messages />
-      <Input/>
+      <Messages chatData={data} />
+      <Input />
     </div>
   );
 };
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,22 +1,19 @@
 import { doc, onSnapshot } from "firebase/firestore";
-import React, { useContext, useEffect, useState } from "react";
-import { ChatContext } from "../context/ChatContext";
+import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import Message from "./Message";
 
-const Messages = () => {
+const Messages = ({ chatData }) => {
   const [messages, setMessages] = useState([]);
-  const { data } = useContext(ChatContext);
-
   useEffect(() => {
-    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+    const unSub = onSnapshot(doc(db, "chats", chatData.chatId), (doc) => {
       doc.exists() && setMessages(doc.data().messages);
     });
 
     return () => {
       unSub();
     };
-  }, [data.chatId]);
+  }, [chatData.chatId]);
 
   return (
     <div className="messages">
