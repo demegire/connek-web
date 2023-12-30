fda7c08c174292e41366d9bc474a07334a5e67a7
tariktrokic <tariktrokic@gmail.com>
2023-12-30 23:54:06 +0300

    Set up mutual home chat layout for profile and initial pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 
-import Sidebar from '../components/Sidebar'
-import Chat from '../components/Chat'
-import AccountSettings from '../components/AccountSettings';
-import SecuritySettings from '../components/SecuritySettings';
-import AttentionSettings from '../components/AttentionSettings';
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+import AccountSettings from "../components/AccountSettings";
+import SecuritySettings from "../components/SecuritySettings";
+import AttentionSettings from "../components/AttentionSettings";
 
-import { SettingsContext } from '../context/SettingsContext';
-import { SelectedSettingContext } from '../context/SelectedSettingContext';
+import { SettingsContext } from "../context/SettingsContext";
+import { SelectedSettingContext } from "../context/SelectedSettingContext";
 
 const Home = () => {
   const { showSettings } = useContext(SettingsContext);
   const { selectedSetting } = useContext(SelectedSettingContext);
   const settingComponents = {
-    'Account': AccountSettings,
-    'Security': SecuritySettings,
-    'Attention': AttentionSettings,
+    Account: AccountSettings,
+    Security: SecuritySettings,
+    Attention: AttentionSettings,
   };
   const SettingComponent = settingComponents[selectedSetting];
 
-  return (
-    <div className='home'>
-      <div className="container">
-        <Sidebar/>
-        {showSettings ? <SettingComponent /> : <Chat/>}
-      </div>
-    </div>
-  )
-}
+  return showSettings ? <SettingComponent /> : <Chat />;
+};
 
-export default Home
+export default Home;
